d2cf6e3361830ff8415de48c9eb3d71c384ed12c
Matt Riley <mattrileyaus@gmail.com>
2022-05-15 23:27:32 +1000

    Unspecified changes

--- src/lib/zora-spa-testrunner.js
+++ src/lib/zora-spa-testrunner.js
@@ -4,41 +4,43 @@ import { createDiffReporter } from 'zora-reporters';
 import glob from 'fast-glob';
 import process from 'process';
 import path from 'path';
-import composeModules from '../../src/compose';
-import composeTesting from '../../testing/compose';
-import testConfig from '../../testing/test-config';
 
-const setup = () => {
-    const { window } = new JSDOM.JSDOM('', { url: 'https://localhost/' });
-    const { helpers } = composeTesting({ window });
-
-    const compose = (config = {}) => {
-        window.document.getElementsByTagName('html')[0].innerHTML = '';
-        delete window.dataLayer;
-        const modules = composeModules({ window }, testConfig, config);
-        modules.startup.start();
-        return modules;
-    };
-
-    return { compose, window, helpers };
-};
-
-const testModuleArgs = setup();
 const [pattern] = process.argv.slice(2);
 const testFiles = glob.sync(pattern);
 const testHarness = createHarness({ indent: true });
 const test = testHarness[process.env.ZORA_ONLY === 'true' ? 'only' : 'test'];
 
-const runTests = filePath => {
-    return test(filePath, async ({ only, skip, ...t }) => {
-        const test = (...args) => t.test(...args);
-        Object.assign(test, { only, skip });
-        const { default: invokeTests } = await import(path.resolve(filePath));
-        return invokeTests({ test, ...testModuleArgs });
-    });
-};
-
 const start = async () => {
+    const composeModules = await import('./src/compose');
+    const composeTesting = await import('./testing/compose');
+    const testConfig = await import('./testing/test-config');
+
+    const setup = () => {
+        const { window } = new JSDOM.JSDOM('', { url: 'https://localhost/' });
+        const { helpers } = composeTesting({ window });
+
+        const compose = (config = {}) => {
+            window.document.getElementsByTagName('html')[0].innerHTML = '';
+            delete window.dataLayer;
+            const modules = composeModules({ window }, testConfig, config);
+            modules.startup.start();
+            return modules;
+        };
+
+        return { compose, window, helpers };
+    };
+
+    const testModuleArgs = setup();
+
+    const runTests = filePath => {
+        return test(filePath, async ({ only, skip, ...t }) => {
+            const test = (...args) => t.test(...args);
+            Object.assign(test, { only, skip });
+            const { default: invokeTests } = await import(path.resolve(filePath));
+            return invokeTests({ test, ...testModuleArgs });
+        });
+    };
+
     await Promise.all(testFiles.map(runTests));
     await testHarness.report({ reporter: createDiffReporter() });
 };

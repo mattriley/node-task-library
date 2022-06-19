67be8dc451ba177906f46faea8e71b148390060d
Matt Riley <mattrileyaus@gmail.com>
2022-06-20 08:36:59 +1000

    Unspecified changes

--- src/lib/readme-file-generator.js
+++ src/lib/readme-file-generator.js
@@ -7,13 +7,25 @@ import process from 'process';
 let codeId = 0;
 
 const packageData = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+const readmeRoot = process.env.READMEGEN_ROOT ?? packageData.homepage;
+const readmeCodeRoot = process.env.READMEGEN_CODE_ROOT ?? `${readmeRoot}/blob/main`;
+const replacements = (process.env.READMEGEN_REPLACE ?? '').split(',').map(str => str.split(' '));
 
 const fetchText = url => child.execSync(`curl ${url} -s`).toString('utf8');
 
-const fetchCode = async (url, opts = {}) => {
-    const code = fetchText(url);
+// const fetchCode = async (url, opts = {}) => {
+//     const codeStr = fetchText(url);
+//     const lang = opts.lang ?? path.extname(url).replace('.', '');
+//     return code(codeStr, lang, url);
+// };
+
+const fetchCode = async (url, opts = {}, callback) => {
+    if (!callback) callback = codeStr => codeStr
+
+    const codeStr = fetchText(url);
     const lang = opts.lang ?? path.extname(url).replace('.', '');
-    return code(code, lang, url);
+    const finalCode = callback(codeStr);
+    return code(finalCode, lang, url);
 };
 
 const readCode = async (paths, opts = {}) => {
@@ -25,17 +37,23 @@ const readCode = async (paths, opts = {}) => {
 };
 
 
-const code = (codeStr, lang, source) => {
+const code = (codeStr, lang = '', source = '') => {
     codeId++;
-    const href = source?.replace('./', '');
+
     const lines = [];
 
+    replacements.forEach(([from, to]) => source = source.replace(from, to));
+
+    const href = source?.replace('./', '');
+
+    const codeLink = href.startsWith('http') ? href : `${readmeCodeRoot}/${href}`;
+
     if (href && !href.startsWith('.')) {
-        lines.push(`###### <p id="code-${codeId}" align="right">${href} • <a href="${href}">View source</a></p>`);
+        lines.push(`###### <p id="code-${codeId}" align="right"><a href="${codeLink}" target="_blank">${href}</a></p>`);
     }
 
     if (lang === 'mermaid') {
-        lines.push(`###### <p id="code-${codeId}" align="right"><em>Can't see the diagram?</em> <a href="${packageData.homepage}#user-content-code-${codeId}">View it on GitHub</a></p>`);
+        lines.push(`###### <p id="code-${codeId}" align="right"><em>Can't see the diagram?</em> <a href="${readmeRoot}#user-content-code-${codeId}" target="_blank">View it on GitHub</a></p>`);
     }
 
     lines.push('```' + lang, codeStr.trim(), '```');

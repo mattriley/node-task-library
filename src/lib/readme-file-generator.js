9e0da2d7dc9230df764bc5f5e6fa92aa8f448d7f
Matt Riley <mattrileyaus@gmail.com>
2022-06-23 21:33:34 +1000

    Unspecified changes

--- src/lib/readme-file-generator.js
+++ src/lib/readme-file-generator.js
@@ -4,8 +4,7 @@ import path from 'path';
 import child from 'child_process';
 import process from 'process';
 
-let codeId = 0;
-
+let linkId = 0;
 const packageData = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
 const readmeRoot = process.env.READMEGEN_ROOT ?? packageData.homepage;
 const readmeCodeRoot = process.env.READMEGEN_CODE_ROOT ?? `${readmeRoot}/blob/main`;
@@ -13,12 +12,21 @@ const readmeCodeRoot = process.env.READMEGEN_CODE_ROOT ?? `${readmeRoot}/blob/ma
 const fetchText = loc => child.execSync(`curl ${loc} -s`).toString('utf8');
 const readText = loc => fs.promises.readFile(loc, 'utf-8');
 
-const fetchCode = async (source, root, webroot) => {
-    const fullSource = path.join(root ?? '', source);
+const fetchCode = async (source, root = './', webroot) => {
+    const fullSource = path.join(root, source);
     const getCode = fullSource.startsWith('http') ? fetchText : readText;
     const code = await getCode(fullSource);
     const lang = path.extname(fullSource).replace('.', '');
     return { code, lang, source, root, webroot };
+};
+
+const renderLink = (href, text) => {
+    return `<a href="${href}">${text}</a>`;
+};
+
+const renderLinkWithId = (href, text) => {
+    linkId++;
+    return `<a id="link-${linkId}" href="${href}#user-content-link-${linkId}">${text}</a>`
 };
 
 
@@ -29,33 +37,26 @@ const renderCode = async (codePromise, lang) => {
     const { code, lang: defaultLang, source, root, webroot } = await codePromise;
     lang = lang ?? defaultLang ?? '';
 
-    codeId++;
-
     const lines = [];
 
-
-    const src = source?.startsWith('./') ? source.replace('./', '') : source
-
-    const href = webroot ? webroot + '/' + src : src;
-
-    // const codeLink = href && href.startsWith('http') ? href : `${readmeCodeRoot}/${href}`;
-
-    const codeLink = href && href.startsWith('http') ? href : `${readmeCodeRoot}/${href}`;
-
-    // if (href && !href.startsWith('.')) {
-    if (href) {
-        lines.push(`###### <p id="code-${codeId}" align="right"><a href="${codeLink}" target="_blank">${href}</a></p>`);
+    if (source) {
+        const text = webroot ? `${webroot}/${source}` : source;
+        const href = `${webroot ?? readmeCodeRoot}/${source}`;
+        const link = renderLink(href, text);
+        lines.push(`###### <p align="right">${link}</p>`);
     }
 
     if (lang === 'mermaid') {
-        lines.push(`###### <p id="code-${codeId}" align="right"><em>Can't see the diagram?</em> <a href="${readmeRoot}#user-content-code-${codeId}" target="_blank">View it on GitHub</a></p>`);
+        const link = renderLinkWithId(readmeRoot, 'View it on GitHub');
+        lines.push(`###### <p align="right"><em>Can't see the diagram?</em> ${link}</p>`);
     }
 
     lines.push('```' + lang, code.trim(), '```');
     return lines.join('\n');
 };
 
-const compose = async (callback, path = './src/compose.js', args = {}) => {
+const compose = async (callback, path = 'src/compose.js', args = {}) => {
+    if (!path.startsWith('.')) path = `./${path}`
     const imported = await import(path);
     const compose = imported?.default ?? imported;
     return callback(await compose(args));

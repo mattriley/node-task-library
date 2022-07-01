5e1a175964d881c96239dcfd96cdaca3caec01ee
Matt Riley <mattrileyaus@gmail.com>
2022-07-01 20:43:49 +1000

    Unspecified changes

---
+++ src/lib/metrics-summary.js
@@ -0,0 +1,14 @@
+import process from 'process';
+
+const data = {
+    cov: require(process.cwd() + `/${process.env.METRICS_COV}`),
+    loc: require(process.cwd() + `/${process.env.METRICS_LOC}`)
+};
+
+const dimensions = ['lines', 'statements', 'functions', 'branches'];
+const percentages = dimensions.map(dim => data.cov.total[dim].pct);
+const cov = Math.min(...percentages);
+const loc = data.loc.sloc;
+const metrics = { cov, loc };
+const json = JSON.stringify(metrics, null, 4);
+console.log(json);

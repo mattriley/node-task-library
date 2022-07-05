8822d9f91fa11534971c2b1c437b4cd6d2274440
Matt Riley <mattrileyaus@gmail.com>
2022-07-05 21:03:52 +1000

    Unspecified changes

--- src/lib/readme-gen.js
+++ src/lib/readme-gen.js
@@ -5,100 +5,109 @@ const child = require('child_process');
 const process = require('process');
 const p = require('../lib/package');
 
-let linkId = 0;
-const readmeTitle = process.env.PROJECT_NAME;
-const readmeRoot = process.env.README_ROOT;
-const readmeCodeRoot = `${readmeRoot}/blob/${process.env.GIT_BRANCH}`;
-
-const lib = {};
-lib.metrics = () => JSON.parse(fs.readFileSync(process.env.METRICS_SUMMARY, 'utf-8'));
-lib.json = obj => JSON.stringify(obj, null, 4);
-lib.readText = loc => fs.promises.readFile(loc, 'utf-8');
-lib.fetchText = loc => child.execSync(`curl ${loc} -s`).toString('utf8');
-lib.renderLink = (href, text) => `<a href="${href}">${text}</a>`;
-lib.renderLinkWithId = (href, text) => `<a id="link-${++linkId}" href="${href}#user-content-link-${linkId}">${text}</a>`;
-
-lib.fetchCode = async (source, root = './', webroot) => {
-    const fullSource = path.join(root, source);
-    const getCode = fullSource.startsWith('http') ? lib.fetchText : lib.readText;
-    const code = await getCode(fullSource);
-    const lang = path.extname(fullSource).replace('.', '');
-    return { code, lang, source, root, webroot };
+const defaultConfig = {
+    template: process.env.README_TEMPLATE,
+    title: process.env.PROJECT_NAME,
+    root: process.env.README_ROOT,
+    codeRoot: `${process.env.README_ROOT}/blob/${process.env.GIT_BRANCH}`,
+    metricsSummary: process.env.METRICS_SUMMARY
 };
 
-lib.compose = async (callback, composeFile = 'src/compose.js', args = {}) => {
-    const imported = await import(path.resolve(composeFile));
-    const compose = imported?.default ?? imported;
-    const composition = compose(args);
-    return callback(composition);
-};
+module.exports = (userConfig = {}) => {
 
-lib.renderCode = async (codePromise, lang, source) => {
-    if (typeof codePromise === 'string') codePromise = { code: codePromise, lang };
-    const { code, lang: defaultLang, source: defaultSource, webroot } = await codePromise;
-    lang = lang ?? defaultLang ?? '';
-    source = source ?? defaultSource;
+    const config = { ...defaultConfig, ...userConfig };
+    const lib = {};
+    let linkId = 0;
 
-    const lines = [];
+    lib.metrics = () => JSON.parse(fs.readFileSync(config.metricsSummary, 'utf-8'));
+    lib.json = obj => JSON.stringify(obj, null, 4);
+    lib.readText = loc => fs.promises.readFile(loc, 'utf-8');
+    lib.fetchText = loc => child.execSync(`curl ${loc} -s`).toString('utf8');
+    lib.renderLink = (href, text) => `<a href="${href}">${text}</a>`;
+    lib.renderLinkWithId = (href, text) => `<a id="link-${++linkId}" href="${href}#user-content-link-${linkId}">${text}</a>`;
 
-    if (source) {
-        const text = webroot ? `${webroot}/${source}` : source;
-        const href = source.startsWith('http') ? source : `${webroot ?? readmeCodeRoot}/${source}`;
-        const link = lib.renderLink(href, text);
-        lines.push(`###### <p align="right">${link}</p>`);
-    }
+    lib.fetchCode = async (source, root = './', webroot) => {
+        const fullSource = path.join(root, source);
+        const getCode = fullSource.startsWith('http') ? lib.fetchText : lib.readText;
+        const code = await getCode(fullSource);
+        const lang = path.extname(fullSource).replace('.', '');
+        return { code, lang, source, root, webroot };
+    };
 
-    if (lang === 'mermaid') {
-        const link = lib.renderLinkWithId(readmeRoot, 'View it on GitHub');
-        lines.push(`###### <p align="right"><em>Can't see the diagram?</em> ${link}</p>`);
-    }
+    lib.compose = async (callback, composeFile = 'src/compose.js', args = {}) => {
+        const imported = await import(path.resolve(composeFile));
+        const compose = imported?.default ?? imported;
+        const composition = compose(args);
+        return callback(composition);
+    };
 
-    lines.push('```' + lang, code.trim(), '```');
-    return lines.join('\n');
-};
+    lib.renderCode = async (codePromise, lang, source) => {
+        if (typeof codePromise === 'string') codePromise = { code: codePromise, lang };
+        const { code, lang: defaultLang, source: defaultSource, webroot } = await codePromise;
+        lang = lang ?? defaultLang ?? '';
+        source = source ?? defaultSource;
 
-lib.renderImage = (path, caption) => {
-    return [
-        '<br />',
-        '<p align="center">',
-        `  <img src="${path}?raw=true" />`,
-        '  <br />',
-        `  <em>${caption}</em>`,
-        '</p>',
-        '<br>',
-    ].join('\n');
-};
+        const lines = [];
 
-lib.renderMetrics = () => {
-    const { cov, sloc, files, deps, devdeps } = lib.metrics();
-    return `<p align="right">
+        if (source) {
+            const text = webroot ? `${webroot}/${source}` : source;
+            const href = source.startsWith('http') ? source : `${webroot ?? config.root}/${source}`;
+            const link = lib.renderLink(href, text);
+            lines.push(`###### <p align="right">${link}</p>`);
+        }
+
+        if (lang === 'mermaid') {
+            const link = lib.renderLinkWithId(config.root, 'View it on GitHub');
+            lines.push(`###### <p align="right"><em>Can't see the diagram?</em> ${link}</p>`);
+        }
+
+        lines.push('```' + lang, code.trim(), '```');
+        return lines.join('\n');
+    };
+
+    lib.renderImage = (path, caption) => {
+        return [
+            '<br />',
+            '<p align="center">',
+            `  <img src="${path}?raw=true" />`,
+            '  <br />',
+            `  <em>${caption}</em>`,
+            '</p>',
+            '<br>',
+        ].join('\n');
+    };
+
+    lib.renderMetrics = () => {
+        const { cov, sloc, files, deps, devdeps } = lib.metrics();
+        return `<p align="right">
     <code>${cov}% cov</code>&nbsp;
     <code>${sloc} sloc</code>&nbsp;
     <code>${files} files</code>&nbsp;
     <code>${deps} deps</code>&nbsp;
     <code>${devdeps} dev deps</code></p>`;
-};
+    };
 
-lib.renderOpening = () => {
-    return [
-        `# ${readmeTitle}`,
-        '',
-        lib.renderMetrics(),
-        '',
-        p.description,
-        '',
-        '## Table of Contents',
-        '',
-        '<!-- START doctoc -->',
-        '<!-- END doctoc -->'
-    ].join('\n');
-};
+    lib.renderOpening = () => {
+        return [
+            `# ${config.title}`,
+            '',
+            lib.renderMetrics(),
+            '',
+            p.description,
+            '',
+            '## Table of Contents',
+            '',
+            '<!-- START doctoc -->',
+            '<!-- END doctoc -->'
+        ].join('\n');
+    };
 
-lib.renderFile = (templateFile, templateData = {}) => {
-    ejs.renderFile(templateFile, { lib: { ...lib, ...templateData } }, { async: true }, async (err, p) => {
-        if (err) throw err;
-        process.stdout.write(await p);
-    });
-};
+    lib.renderFile = (templateData = {}) => {
+        ejs.renderFile(config.template, { lib: { ...lib, ...templateData } }, { async: true }, async (err, p) => {
+            if (err) throw err;
+            process.stdout.write(await p);
+        });
+    };
 
-module.exports = lib;
+    return lib;
+};
--- src/node-tasks/readme-gen.js
+++ src/node-tasks/readme-gen.js
@@ -1,5 +1,1 @@
-const process = require('process');
-
-const lib = require('../lib/readme-gen');
-const [templateFile] = process.argv.slice(2);
-lib.renderFile(templateFile);
+require('../lib/readme-gen')().renderFile();

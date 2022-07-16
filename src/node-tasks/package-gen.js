5b374aab62ac22e3681b442c7ac4c5bc7a938aea
Matt Riley <mattrileyaus@gmail.com>
2022-07-16 10:47:58 +1000

    Unspecified changes

--- src/node-tasks/package-gen.js
+++ src/node-tasks/package-gen.js
@@ -2,6 +2,7 @@ const glob = require('fast-glob');
 const path = require('path');
 const p = require('../lib/package');
 const logJson = require('../lib/log-json');
+const process = require('process');
 
 const binFiles = glob.sync('./bin/*');
 
@@ -10,7 +11,24 @@ p.bin = binFiles.reduce((acc, f) => {
     return { ...acc, [name]: f };
 }, {});
 
-const seq = ['name', 'version', 'description', 'keywords', 'author', 'license', 'homepage', 'repository', 'bugs', 'private', 'type', 'main', 'bin', 'files', 'scripts', 'dependencies', 'devDependencies'];
+p.author = {
+    name: process.env.AUTHOR_NAME,
+    email: process.env.AUTHOR_EMAIL,
+    url: process.env.AUTHOR_URL
+};
+
+p.bugs = {};
+if (process.env.ISSUES_URL) p.bugs.url = process.env.ISSUES_URL;
+if (process.env.ISSUES_EMAIL) p.bugs.email = process.env.ISSUES_EMAIL;
+
+if (p.homepage) delete p.homepage;
+if (process.env.GITHUB_REPO_URL) p.homepage = process.env.GITHUB_REPO_URL;
+
+if (!p.scripts) p.scripts = {};
+p.scripts.test = 'npx task test';
+p.scripts.start = 'npx task start';
+
+const seq = ['name', 'version', 'description', 'homepage', 'license', 'keywords', 'author', 'repository', 'bugs', 'private', 'type', 'main', 'bin', 'files', 'scripts', 'dependencies', 'devDependencies'];
 const unk = Object.keys(p).filter(key => !seq.includes(key));
 
 const newEntries = [...seq, ...unk].reduce((acc, key) => {

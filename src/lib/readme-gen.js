1840a4699b96a2ee7b6969486183d465902a4fe2
Matt Riley <mattrileyaus@gmail.com>
2022-07-16 22:23:57 +1000

    Unspecified changes

--- src/lib/readme-gen.js
+++ src/lib/readme-gen.js
@@ -7,7 +7,7 @@ const p = require('../lib/package');
 
 const defaultConfig = {
     template: process.env.README_TEMPLATE,
-    title: process.env.PACKAGE_NAME,
+    title: process.env.DISPLAY_NAME,
     baseUrl: process.env.README_BASE_URL,
     gitBranch: process.env.GIT_BRANCH,
     metricsSummary: process.env.METRICS_SUMMARY

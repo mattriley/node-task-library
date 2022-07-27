bdfdf8ca1391ed23047260e2d87fae5308f4ebe6
Matt Riley <mattrileyaus@gmail.com>
2022-07-28 07:22:00 +1000

    Unspecified changes

--- src/node-tasks/package-gen.js
+++ src/node-tasks/package-gen.js
@@ -31,6 +31,12 @@ p.scripts.test = 'npx task test';
 p.scripts.start = 'npx task start';
 p.scripts.deploy = 'npx task deploy';
 
+if (!p.private) {
+    p.scripts.preversion = 'npx task pre-commit';
+    p.scripts.postversion = 'git push && git push --tags';
+}
+
+
 const seq = ['name', 'version', 'description', 'license', 'homepage', 'repository', 'author', 'bugs', 'bin', 'files', 'main', 'type', 'private', 'scripts', 'dependencies', 'devDependencies', 'keywords'];
 const unk = Object.keys(p).filter(key => !seq.includes(key));
 

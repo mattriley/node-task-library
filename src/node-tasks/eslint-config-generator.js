cabab6f4e81f2bdb8f141ffd98556e9457d06fcf
Matt Riley <mattrileyaus@gmail.com>
2022-07-28 19:36:21 +1000

    Unspecified changes

--- src/node-tasks/eslint-config-generator.js
+++ src/node-tasks/eslint-config-generator.js
@@ -1,14 +1,33 @@
+const process = require('process');
 const p = require('../lib/package');
 const logJson = require('../lib/log-json');
 const eslintConfig = require('../configs/eslintrc.json');
+
+const dependsOn = dep => p.devDependencies?.[dep] || p.dependencies?.[dep];
 
 if (p.type === 'module') {
     eslintConfig.parserOptions.sourceType = 'module';
 }
 
-if (p.devDependencies?.react || p.dependencies?.react) {
+if (dependsOn('react')) {
     eslintConfig.extends.push('plugin:react/recommended');
     eslintConfig.extends.push('plugin:react/jsx-runtime');
+}
+
+if (dependsOn('jest')) {
+    if (!eslintConfig.overrides) eslintConfig.overrides = [];
+
+    eslintConfig.overrides.push(
+        {
+            files: [process.env.TEST_PATTERN],
+            plugins: ['jest'],
+            extends: ['plugin:jest/recommended'],
+            rules: {
+                'jest/require-top-level-describe': 'off',
+                'jest/prefer-expect-assertions': 'off'
+            }
+        }
+    );
 }
 
 logJson(eslintConfig);

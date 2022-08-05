9a86101556049669d1890c31a7501b34f76e1236
Matt Riley <mattrileyaus@gmail.com>
2022-08-05 22:17:48 +1000

    Unspecified changes

--- src/bin/port-gen.js
+++ src/bin/port-gen.js
@@ -3,6 +3,7 @@ const [str] = process.argv.slice(2);
 const nums = [...str].map(s => s.charCodeAt(0)).join('');
 const step = Math.trunc(nums.length / 5);
 const ints = [];
-for (let i = 1; i < 5; i++) { ints.push(nums[i * step] || 1); }
+for (let i = 1; i < 5; i++) { ints.push(nums[i * step]); }
+if (ints[0] === '0') ints[0] = '1';
 const port = ints.join('');
 console.log(port);

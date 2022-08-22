8b35e96bd69d7c95fe181147e2057eafb89145a0
Matt Riley <mattrileyaus@gmail.com>
2022-08-22 22:19:12 +1000

    Unspecified changes

--- src/bash/lib/load-vars.sh
+++ src/bash/lib/load-vars.sh
@@ -3,13 +3,12 @@ function load_vars {
     source "./task-vars" 2> /dev/null
     root=${ROOT_OVERRIDE:-"$ROOT"}
     [ "$root" ] && source "$root/task-vars" 2> /dev/null
-    vars=$(export_functions "$TASK_LIBRARY_ROOT/src/bash/vars")
 
     while IFS= read -r name; do
         override_name="${name}_OVERRIDE"
         [ "${!override_name}" ] && export $name="${!override_name}"
         [ -z "${!name}" ] && export $name="$($name)"
-    done <<< "$vars"
+    done <<< "$VAR_NAMES"
 
     env_after="$(env)"
     export VARS=$(sort <(echo "$env_before" ) <(echo "$env_after") | uniq -u)

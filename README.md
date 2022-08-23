2facc6f369eebd24293dcba083566bf307528103
Matt Riley <mattrileyaus@gmail.com>
2022-08-23 21:55:19 +1000

    Unspecified changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Task Library
 
-<p align="right"><code>1399 sloc</code>&nbsp;<code>142 files</code>&nbsp;<code>5 deps</code>&nbsp;<code>11 dev deps</code></p>
+<p align="right"><code>1408 sloc</code>&nbsp;<code>142 files</code>&nbsp;<code>5 deps</code>&nbsp;<code>11 dev deps</code></p>
 
 A collection of bash scripts to support build, test, deploy and chores associated with developing and maintaining Node apps.
 
--- src/bash/lib/load-vars.sh
+++ src/bash/lib/load-vars.sh
@@ -8,13 +8,31 @@ function load_vars {
     source "./task-vars" 2> /dev/null
     root=${ROOT_OVERRIDE:-"$ROOT"}
     [ "$root" ] && source "$root/task-vars" 2> /dev/null
+    env_after="$(env)"
+    export VARS1=$(sort <(echo "$env_before" ) <(echo "$env_after") | uniq -u | sed 's;=.*;;')
 
+    
     while IFS= read -r name; do
         # echo "$name"
         override_name="${name}_OVERRIDE"
         [ "${!override_name}" ] && export $name="${!override_name}"
         [ -z "${!name}" ] && export $name="$($name)"
     done <<< "$var_names"
+
+    stage_upper=$(echo "$STAGE" | tr '[:lower:]' '[:upper:]')
+    while IFS= read -r name; do
+        # if [[ "$name" =~ __$stage_upper$ ]]; then
+        if [[ "$name" =~ __$stage_upper$ ]]; then
+            # echo "$name"
+            stageless=${name/__$stage_upper/}
+            export $stageless=${!name}
+        fi
+
+        # if [[ "$name" =~ __ ]]; then
+        #     unset $name
+        # fi
+    done <<< "$VARS1"
+
 
     env_after="$(env)"
     export VARS=$(sort <(echo "$env_before" ) <(echo "$env_after") | uniq -u)

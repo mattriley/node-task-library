89b210347ea3ed070116f8ac0eef49d810eccb00
Matt Riley <mattrileyaus@gmail.com>
2022-08-23 21:02:17 +1000

    Unspecified changes

--- src/bash/vars/03-meta.sh
+++ src/bash/vars/03-meta.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-function SEP {
-    echo " | "
-}
-
-function PRINT_VARS {
-    echo "ROOT | DISPLAY_NAME | PACKAGE_NAME | STAGE | PORT"
-}
 
 function STAGE {
     echo "local"
-}
-
-function VERSION_DATE {
-    echo "$(date)"
 }
 
 function PACKAGE_NAME {
@@ -24,26 +13,26 @@ function DISPLAY_NAME {
     node -p "require('lodash').startCase('$PACKAGE_NAME')"
 }
 
-function COPY_MODULES {
-    echo ""
-}
-
 function NODE_MODULES_LINKS {
     echo "$PACKAGE_ROOT/node_modules_links"
+}
+
+function PRINT_VARS {
+    echo "ROOT | DISPLAY_NAME | PACKAGE_NAME | STAGE | PORT"
 }
 
 function NODE_MODULES_LINK_PACKAGES {
     echo "module-composer | module-indexgen | task-library"
 }
 
-
-
 function ITERMOCIL_TASKS {
     echo "edit | test-watch | start | barrel-gen-watch"
 }
 
-
-
 function TASK_PATHS {
     echo "$TASKS | $TASK_LIBRARY_ROOT/src/bash/tasks"
+}
+
+function COPY_MODULES {
+    echo ""
 }

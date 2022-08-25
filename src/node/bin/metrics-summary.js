08144cb4e7cdd39ce3be82df2b9f472ec836c0b3
Matt Riley <mattrileyaus@gmail.com>
2022-08-25 12:02:19 +1000

    Unspecified changes

--- src/node/bin/metrics-summary.js
+++ src/node/bin/metrics-summary.js
@@ -3,13 +3,22 @@ const process = require('process');
 const logJson = require('../lib/log-json');
 const p = require('../lib/package');
 
+const tryRequire = p => {
+    try {
+        return require(p);
+    }
+    catch {
+        return {};
+    }
+};
+
 const data = {
-    cov: require(path.resolve(process.env.METRICS_COV)),
-    loc: require(path.resolve(process.env.METRICS_LOC))
+    cov: tryRequire(path.resolve(process.env.METRICS_COV)),
+    loc: tryRequire(path.resolve(process.env.METRICS_LOC))
 };
 
 const dimensions = ['lines', 'statements', 'functions', 'branches'];
-const percentages = dimensions.map(dim => data.cov.total[dim].pct);
+const percentages = dimensions.map(dim => data.cov.total?.[dim].pct);
 const cov = Math.min(...percentages) || 0;
 const sloc = data.loc.SUM.code;
 const files = data.loc.SUM.nFiles;

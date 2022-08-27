2c1bdba577c7ec018746d13b98d511c5400c39a6
Matt Riley <mattrileyaus@gmail.com>
2022-08-27 21:49:01 +1000

    Unspecified changes

--- src/bash/util/list-join.sh
+++ src/bash/util/list-join.sh
@@ -1,6 +1,6 @@
 function list_join {
 
-    local sep=${2:-$SEP}
+    local sep=${OUTSEP:-$SEP}
     echo "${1//${IFS:0:1}/$sep}"
     
 }
--- src/bash/util/list-map.sh
+++ src/bash/util/list-map.sh
@@ -3,13 +3,12 @@ function list_map {
     local IFS="$SEP"
     local arr=($1)
     local func=$2
-    local out_sep="$3"
     local res=()
 
     for item in "${arr[@]}"; do 
         res+=($($func "$item"))
     done
     
-    list_join "${res[*]}" "$out_sep" # joins on first character of $IFS (space)
+    list_join "${res[*]}"
 
 }

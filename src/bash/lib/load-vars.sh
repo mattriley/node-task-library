9ad9f1731f71c7216e6476e8e162a0699fc3c07d
Matt Riley <mattrileyaus@gmail.com>
2022-08-27 22:11:00 +1000

    Unspecified changes

--- src/bash/lib/load-vars.sh
+++ src/bash/lib/load-vars.sh
@@ -2,44 +2,36 @@
 
 function load_vars {
     [ "$VARS" ] && return 0
-    vars_path="$TASK_LIBRARY_ROOT/src/bash/vars"
+    local vars_path="$TASK_LIBRARY_ROOT/src/bash/vars"
     export_functions "$vars_path" > /dev/null
-    internal_var_names=$(export_functions "$vars_path")
+    local internal_var_names=$(export_functions "$vars_path")
 
-    env_before="$(env)"
+    local env_before="$(env)"
     source "./task-vars" 2> /dev/null
-    root=${ROOT_OVERRIDE:-"$ROOT"}
+    local root=${ROOT_OVERRIDE:-"$ROOT"}
     [ "$root" ] && source "$root/task-vars" 2> /dev/null
-    env_after="$(env)"
-    external_var_names=$(uniq_vars "$env_before" "$env_after" | sed 's;=.*;;')
-
-    # while IFS= read -r name; do
-    #     override_name="${name}_OVERRIDE"
-    #     [ "${!override_name}" ] && export $name="${!override_name}"
-    #     val="$($name)"
-    #     val=${val/$SEP/ }
-    #     [ -z "${!name}" ] && export $name="$val"
-    # done <<< "$internal_var_names"
+    local env_after="$(env)"
+    local external_var_names=$(uniq_vars "$env_before" "$env_after" | sed 's;=.*;;')
 
     while IFS= read -r name; do
-        override_name="${name}_OVERRIDE"
+        local override_name="${name}_OVERRIDE"
         local val="${!name}"
         [ "${!override_name}" ] && val="${!override_name}"        
         [ -z "${!name}" ] && val="$($name)"
-        val=${val//$SEP/ }
+        local val=${val//$SEP/ }
         export "$name"="$val"
     done <<< "$internal_var_names"    
 
-    stage_upper=$(echo "$STAGE" | tr '[:lower:]' '[:upper:]')
-    staged_var_names=$(echo "$external_var_names" | sed "s;=.+__$stage_upper;;")
+    local stage_upper=$(echo "$STAGE" | tr '[:lower:]' '[:upper:]')
+    local staged_var_names=$(echo "$external_var_names" | sed "s;=.+__$stage_upper;;")
 
     while IFS= read -r name; do
-        base_name=${name/__$stage_upper/}
+        local base_name=${name/__$stage_upper/}
         export "$base_name"="${!name}"
     done <<< "$staged_var_names"
 
-    env_after="$(env)"
+    local env_after="$(env)"
     export VARS=$(uniq_vars "$env_before" "$env_after")
     echo
-    npx task print-vars
+    run_task print-vars
 }

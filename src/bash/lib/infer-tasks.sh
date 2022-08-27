cb0658009b713b8d8938d781b081660a81e7ab6f
Matt Riley <mattrileyaus@gmail.com>
2022-08-27 21:22:16 +1000

    Unspecified changes

--- src/bash/lib/infer-tasks.sh
+++ src/bash/lib/infer-tasks.sh
@@ -1,36 +1,19 @@
 function infer_tasks {
 
-    task_name="$1"
-    func_prefix="f"
-    default_task="$2"
-
-    func_names=""
-    x=1
-    while [ "$(type -t "$func_prefix$x")" = 'function' ]
+    local task_name="$1"
+    local default_task="$2"
+    local func_prefix="f"
+    local func_names=""
+    local n=1
+    
+    while [ "$(type -t "$func_prefix$n")" = 'function' ]
     do
-        func_names+=" $func_prefix$x"
-        x=x+1
+        func_names+=" $func_prefix$n"
+        n=n+1
     done
 
     function mapper { $1; unset $1; }
     tasks=$(map "$func_names" mapper)
-
-    # echo "1 $tasks"
-
-    #function predicate1 { echo "&&&&&&"; task_exists "$1"; echo "$?"; return "$?"; }
-    #tasks=$(filter "$tasks" predicate1)
-    num_tasks=$(length "$tasks")
-
     [ -z "$tasks" ] && echo "$default_task" || echo "$tasks"
-
-    # echo "$tasks"
-
-    # echo "2 $tasks"
-
-    # [ -z "$tasks" ] && warn "Unable to infer $task_name task" && exit 0
-
-    # echo "   Inferred ${BOLD}$tasks${NORM}"
-
-    # run_tasks "$tasks"
 
 }
--- src/bash/lib/infer-test-task.sh
+++ src/bash/lib/infer-test-task.sh
@@ -1,14 +1,11 @@
 function infer_test_task {
 
-    suffix="$1"
-    default_task="$2"
-
-    # [ -z "$TEST_RUNNER" ] && warn "No test runner found" && return 1
+    local task_name="$1"
+    local default_task="$2"
 
     [ -z "$TEST_RUNNER" ] && echo "$default_task" && return 0
 
-    function f1 { [ -z "$suffix" ] && echo "$TEST_RUNNER" || echo "$TEST_RUNNER-$suffix"; }
-
-    infer_tasks "$suffix" "$default_task"
+    function f1 { [ -z "$task_name" ] && echo "$TEST_RUNNER" || echo "$TEST_RUNNER-$task_name"; }
+    infer_tasks "$task_name" "$default_task"
 
 }

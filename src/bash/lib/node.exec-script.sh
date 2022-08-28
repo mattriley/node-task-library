05a980ec45cb826c78ff87c9487ae356919f07d6
Matt Riley <mattrileyaus@gmail.com>
2022-08-29 09:28:12 +1000

    Unspecified changes

--- src/bash/lib/node.exec-script.sh
+++ src/bash/lib/node.exec-script.sh
@@ -2,6 +2,19 @@
 
 function node.exec_script {
 
-    node "$TASK_LIBRARY_ROOT/src/node/bin/$1" "${@:2}"
-    
+    local node_script="$TASK_LIBRARY_ROOT/src/node/bin/$1"
+    [ ! -f "$node_script" ] && node_script="$1"
+    local node_command="node"
+    local node_options=""
+
+    if npm.has_dev_dependency "@node-loader/babel"; then
+        node_options="--experimental-loader @node-loader/babel"
+    elif npm.has_dev_dependency "@babel/core"; then
+        node_command="babel-node"
+    fi
+
+    # shellcheck disable=SC2086
+    "$node_command" $node_options "$node_script" "${@:2}"
+    # NODE_OPTIONS="$node_options" "$node_command" "$node_script" "${@:2}"
+
 }
--- src/bash/tasks/readme-gen.sh
+++ src/bash/tasks/readme-gen.sh
@@ -2,23 +2,9 @@
   
 set -e
 
-[ ! -f "$README_TEMPLATE" ] && exit 0
+[ ! -f "$README_TEMPLATE" ] && warn "$README_TEMPLATE not found" && exit 0
 
- node_command="node"
- node_options=""
-
-if npm.has_dev_dependency "@node-loader/babel"
-then
-    node_options="--experimental-loader @node-loader/babel"
-    export BABEL_OMIT_PRESETS="@babel/preset-env"
-elif npm.has_dev_dependency "@babel/core"
-then
-    node_command="babel-node"
-fi
-
-NODE_OPTIONS="$node_options" \
-$node_command "$README_GEN" > "$README"
-
+node.exec_script "$README_GEN" > "$README"
 npx task doctoc
 
 git add "$README"

a39a55dbd769c477a61ecc12ff9a5de704411540
Matt Riley <mattrileyaus@gmail.com>
2022-08-28 20:16:56 +1000

    Unspecified changes

--- src/bash/lib/find-task-file.sh
+++ src/bash/lib/find-task-file.sh
@@ -3,7 +3,7 @@ function find_task_file {
     local task_name="$1"
     [ -z "$task_name" ] && echo "Task name is required" && exit 1
     
-    function callback { echo "$1/$task_name"; }
+    function callback { echo "$1/$task_name $1/$task_name.sh"; }
     paths=$(list.map "$TASK_PATHS")
 
     function callback { [ -f "$1" ]; }

aab78dc51c71754bea0e0597dc2e5f08718bd963
Matt Riley <mattrileyaus@gmail.com>
2022-09-02 08:56:28 +1000

    Unspecified changes

--- src/bash/modules/tasks/cov-report-infer.sh
+++ src/bash/modules/tasks/cov-report-infer.sh
@@ -4,6 +4,6 @@ function tasks.cov_report_infer {
 
     set -e
 
-    lib.run_tasks "$(infer_test_task "cov-report" "$DEFAULT_COV_REPORT_TASK")"
+    lib.run_tasks "$(lib.infer_test_task "cov-report" "$DEFAULT_COV_REPORT_TASK")"
 
 }

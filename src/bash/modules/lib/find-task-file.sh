26002464992a843bfe50465163effdf0232ec7e6
Matt Riley <mattrileyaus@gmail.com>
2022-09-02 16:14:13 +1000

    Unspecified changes

--- src/bash/modules/lib/find-task-file.sh
+++ src/bash/modules/lib/find-task-file.sh
@@ -6,7 +6,7 @@ function lib.find_task_file {
 
     [ -z "$task_name" ] && util.warn "Task name is required" && return 1
 
-    function callback { [ -f "$1" ]; }
-    list.find "$1/$task_name | $1/$task_name.sh"
+    function callback { [ -f "$TASKS/$1" ]; }
+    list.find "$task_name | $task_name.sh"
 
 }

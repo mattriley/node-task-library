24a4e04fb42ed6cd96845cdb99251c26dd1cb377
Matt Riley <mattrileyaus@gmail.com>
2022-09-03 19:42:38 +1000

    Unspecified changes

--- src/bash/modules/tasks/node-test.sh
+++ src/bash/modules/tasks/node-test.sh
@@ -2,10 +2,8 @@
 
 function tasks.node_test {
 
-    ui.warn "Not implemented"
-
-    # test_files=$(node -p "require('fast-glob').sync('$TEST_PATTERN').join('$SEP')")
-    # function node_test { node --experimental-specifier-resolution=node "$1"; }
-    # list.each "$test_files"
+    test_files=$(node -p "require('fast-glob').sync('$TEST_PATTERN').join('$SEP')")
+    function node_test { node --experimental-specifier-resolution=node "$1"; }
+    list.each "$test_files"
 
 }

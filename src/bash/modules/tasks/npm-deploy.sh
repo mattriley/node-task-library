bcba6ac7e5df76265da413d6884435c54efeea27
Matt Riley <mattrileyaus@gmail.com>
2022-09-03 20:42:57 +1000

    Unspecified changes

--- src/bash/modules/tasks/npm-deploy.sh
+++ src/bash/modules/tasks/npm-deploy.sh
@@ -2,6 +2,7 @@
 
 function tasks.npm_deploy {
 
+    ui.newline
     ui.info "${BOLD}$NPM_BUMP_VERSION${NORM} version will be bumped"
 
     ui.ask_ok || return 1
@@ -11,4 +12,6 @@ function tasks.npm_deploy {
         && git push \
         && git push --tags
 
+    ui.newline
+    
 }

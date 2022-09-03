2f3d09c31074b1307a13d3828da576528cdc3c35
Matt Riley <mattrileyaus@gmail.com>
2022-09-03 20:41:24 +1000

    Unspecified changes

--- src/bash/modules/lib/infer-and-run-test-tasks.sh
+++ src/bash/modules/lib/infer-and-run-test-tasks.sh
@@ -4,6 +4,6 @@ function lib.infer_and_run_test_tasks {
 
     local tasks; tasks=$(lib.infer_test_tasks "$@")
     [ "$tasks" ] && lib.run_tasks "$tasks" && return
-    ui.warn "No tasks inferred"
+    ui.task_warn "No tasks inferred"
 
 }

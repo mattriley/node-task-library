648b77cf9dc3bf93f677caddf5ffe2389ad41888
Matt Riley <mattrileyaus@gmail.com>
2022-09-04 18:46:01 +1000

    Unspecified changes

--- src/bash/modules/lib/find-task-file.sh
+++ src/bash/modules/lib/find-task-file.sh
@@ -3,7 +3,7 @@
 function lib.find_task_file {
 
     local task_name="$1"
-    function callback { [ -f "$TASKS/$1" ]; }
-    ( cd "$ROOT" && list.find "$task_name | $task_name.sh" )
+    function callback { [ -f "$1" ]; }
+    ( cd "$ROOT" && list.find "$TASKS/$task_name | $TASKS/$task_name.sh" )
 
 }

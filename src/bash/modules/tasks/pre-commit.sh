bed1db44c81469c5edcb82bf18e4bed275e1fd8a
Matt Riley <mattrileyaus@gmail.com>
2022-09-06 08:53:28 +1000

    Unspecified changes

--- src/bash/modules/tasks/pre-commit.sh
+++ src/bash/modules/tasks/pre-commit.sh
@@ -2,16 +2,12 @@
 
 function tasks.pre_commit {
 
-    # lib.run_task noop
-
     lib.run_task config-gen
     lib.run_task code-gen
-    lib.run_task lint --fix
+    lib.run_task lint
     lib.run_task cov
     lib.run_task sloc
     lib.run_task metrics-summary
     lib.run_task readme-gen
-
-    git add .
 
 }

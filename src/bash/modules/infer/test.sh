fb6cfbad6627e379a8f835a8551c823ee52fb49a
Matt Riley <mattrileyaus@gmail.com>
2022-09-05 21:07:18 +1000

    Unspecified changes

---
+++ src/bash/modules/infer/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+function infer.test {
+
+    lib.infer_test_tasks "" "$DEFAULT_TEST_TASK"
+
+}
--- src/bash/modules/tasks/test-infer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-function tasks.test_infer {
-
-    lib.infer_and_run_test_tasks "" "$DEFAULT_TEST_TASK"
-
-}

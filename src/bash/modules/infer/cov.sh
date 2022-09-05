c54124153f9f16b2aaf3c4332f0c983aaa8b080f
Matt Riley <mattrileyaus@gmail.com>
2022-09-05 17:34:51 +1000

    Unspecified changes

---
+++ src/bash/modules/infer/cov.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+function tasks.cov_infer {
+
+    lib.infer_test_tasks "cov" "$DEFAULT_COV_TASK"
+
+}
--- src/bash/modules/tasks/cov-infer.sh
+++ src/bash/modules/tasks/cov-infer.sh
@@ -2,6 +2,6 @@
 
 function tasks.cov_infer {
 
-    lib.infer_and_run_test_tasks "cov" "$DEFAULT_COV_TASK"
+    lib.run_tasks $(infer.cov)
 
 }

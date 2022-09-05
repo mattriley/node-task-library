4870bfb03516143dca47481577bf22b0cb8e1b86
Matt Riley <mattrileyaus@gmail.com>
2022-09-05 21:36:39 +1000

    Unspecified changes

---
+++ src/bash/modules/infer/config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function infer.config {
+
+    function f1 { echo "gitignore-gen"; }
+    function f2 { echo "package-gen"; }
+    function f3 { npm.has_dev_dependency "react" && echo "babel-config-gen"; }
+    function f4 { npm.has_dev_dependency "jest" && echo "jest-config-gen"; }
+    function f5 { npm.has_dev_dependency "eslint" && echo "eslint-config-gen"; }
+    lib.infer_tasks
+
+}

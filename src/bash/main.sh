61115e8765d5fc7738a4a3ed97f89b0d64ad3657
Matt Riley <mattrileyaus@gmail.com>
2022-09-06 20:25:13 +1000

    Unspecified changes

--- src/bash/main.sh
+++ src/bash/main.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # shellcheck disable=SC1090,SC1091
 
+set -o pipefail
+
 source "$(dirname "${BASH_SOURCE[0]}")/globals.sh"
 
 function main {
-
-    set -o pipefail
-
-    [ "$VARS" ] && export IS_SUBTASK="true"
 
     for module_path in "$TASK_LIBRARY_ROOT/src/bash/modules"/*; do
         for script_path in "$module_path"/*.sh; do
@@ -15,12 +13,13 @@ function main {
         done
     done
 
+    { [ "$VARS" ] && export IS_SUBTASK="true"; } || startup.load_vars
+
     trap startup.on_term SIGTERM SIGINT
     trap startup.on_exit EXIT
 
     local task_name=${1:-default}
-    [ -z "$VARS" ] && startup.load_vars 
-    [ ! "$task_name" = "vars" ] && [ -z "$VARS" ] && tasks.print_vars
+    [ ! "$task_name" = "vars" ] && [ -z "$IS_SUBTASK" ] && tasks.print_vars
     lib.run_task "$task_name"
 
 }
--- src/bash/modules/startup/on-exit.sh
+++ src/bash/modules/startup/on-exit.sh
@@ -2,14 +2,13 @@
 
 function startup.on_exit {
     
-    :
-    # local exit_code="$?"
-    # local failure="${RED}FAILURE${NORM}"
-    # local success="${GRE}SUCCESS${NORM}"
+    local exit_code="$?"
+    local failure="${RED}FAILURE${NORM}"
+    local success="${GRE}SUCCESS${NORM}"
 
-    # ui.newline
-    # [ "$exit_code" -gt 0 ] && echo "$failure" && exit 1
-    # # [ "$IS_SUBTASK" ] && return
-    # echo "$success"
+    ui.newline
+    [ "$exit_code" -gt 0 ] && echo "$failure" && exit 1
+    [ "$IS_SUBTASK" ] && return
+    echo "$success"
 
 }

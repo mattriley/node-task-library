df134dae6e2bd571853c69c9c461169d54a19856
Matt Riley <mattrileyaus@gmail.com>
2022-09-06 18:51:17 +1000

    Unspecified changes

--- src/bash/modules/lib/infer-tasks.sh
+++ src/bash/modules/lib/infer-tasks.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
+# shellcheck disable=SC2005
 
 function lib.infer_tasks {
 
     local default_task="$1"
-
-    function callback {
-        # shellcheck disable=SC2005
-        echo "$($1)"
-        unset "$1"
-    }
-
+    function callback { echo "$($1)"; unset "$1"; }
     local tasks; tasks=$(list.map "$(util.list_of_func)")
     [ -z "$tasks" ] && tasks="$default_task"
     [ -z "$tasks" ] && ui.task_warn "No tasks inferred"
--- src/bash/modules/lib/run-tasks.sh
+++ src/bash/modules/lib/run-tasks.sh
@@ -3,7 +3,7 @@
 function lib.run_tasks {
 
     local tasks="$1"
-    local result; result=0;
+    local result=0;
     function callback { ! lib.run_task "$1" && result=1; }
     list.each "$tasks"
     return $result;

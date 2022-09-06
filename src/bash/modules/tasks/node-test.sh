406fdb1412ff0e588f4f4b05f0faabec33c55aa0
Matt Riley <mattrileyaus@gmail.com>
2022-09-06 19:05:27 +1000

    Unspecified changes

--- src/bash/modules/tasks/node-test.sh
+++ src/bash/modules/tasks/node-test.sh
@@ -2,8 +2,10 @@
 
 function tasks.node_test {
 
-    function callback { node.exec_script "$1"; }
+    local result=0;
+    function callback { ! node.exec_script "$1" && result=1; }
     local test_files; test_files=$(node -p "require('fast-glob').sync('$TEST_PATTERN').join('$SEP')") && \
     list.each "$test_files"
+    return $result;
 
 }

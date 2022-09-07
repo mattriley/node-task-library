0cb4330929069332ba2789ed6218787d6353034d
Matt Riley <mattrileyaus@gmail.com>
2022-09-07 13:12:19 +1000

    Unspecified changes

--- src/bash/modules/tasks/chokidar-test-watch.sh
+++ src/bash/modules/tasks/chokidar-test-watch.sh
@@ -3,6 +3,8 @@
 
 function tasks.chokidar_test_watch {
 
+    [ -z "$TEST_WATCH_PATHS" ] && ui.task_warn "No watch paths exist" && return
+
     ui.newline && \
     chokidar $TEST_WATCH_PATHS -c "$TASK_LIBRARY_ROOT/bin/task test"
 

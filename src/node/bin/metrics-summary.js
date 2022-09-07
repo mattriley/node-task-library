6f76c1233eb19819d934558a4e80062c08706f0c
Matt Riley <mattrileyaus@gmail.com>
2022-09-07 14:56:54 +1000

    Unspecified changes

--- src/node/bin/metrics-summary.js
+++ src/node/bin/metrics-summary.js
@@ -20,8 +20,8 @@ const data = {
 const dimensions = ['lines', 'statements', 'functions', 'branches'];
 const percentages = dimensions.map(dim => data.cov.total?.[dim].pct);
 const cov = Math.min(...percentages) || 0;
-const sloc = data.loc?.SUM.code;
-const files = data.loc?.SUM.nFiles;
+const sloc = data.loc.SUM?.code;
+const files = data.loc.SUM?.nFiles;
 const deps = Object.keys(p.dependencies ?? {}).length;
 const devdeps = Object.keys(p.devDependencies ?? {}).length;
 const metrics = { cov, sloc, files, deps, devdeps };

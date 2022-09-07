ada59a00a5a3fae061d97211c61c247905517c30
Matt Riley <mattrileyaus@gmail.com>
2022-09-07 13:21:20 +1000

    Unspecified changes

--- src/bash/modules/tasks/install-npm-packages.sh
+++ src/bash/modules/tasks/install-npm-packages.sh
@@ -8,12 +8,13 @@ function tasks.install_npm_packages {
     function f3 { [ -d "$MODULES" ] && echo "-D module-indexgen"; }
     function f4 { [ -f "$SERVER" ] && echo "express cors ajv ajv-formats"; }
     function f5 { [ -f "$SERVER" ] && echo "-D nodemon"; }
-    function f6 { [ -f "$README_TEMPLATE" ] && echo "-D ejs doctoc cloc"; }
-    function f7 { [ -f "$SERVERLESS_CONFIG" ] && echo "-D serverless aws-sdk"; }
-    function f8 { npm.has_dev_dependency "react" && echo "-D @babel/core @babel/preset-react"; }
-    function f9 { npm.has_dev_dependency "@babel/core" && echo "-D @babel/preset-env @babel/node"; }
-    function f10 { npm.has_dev_dependency "jest" && echo "-D jest-environment-jsdom"; }
-    function f11 { npm.has_dev_dependency "eslint" && echo "-D $(printf "eslint-plugin-%s " "$ESLINT_PLUGINS")"; }
+    function f6 { [ -d "$STATIC" ] && [ ! -d "$SRC" ] && echo "-D serve"; }
+    function f7 { [ -f "$README_TEMPLATE" ] && echo "-D ejs doctoc cloc"; }
+    function f8 { [ -f "$SERVERLESS_CONFIG" ] && echo "-D serverless aws-sdk"; }
+    function f9 { npm.has_dev_dependency "react" && echo "-D @babel/core @babel/preset-react"; }
+    function f10 { npm.has_dev_dependency "@babel/core" && echo "-D @babel/preset-env @babel/node"; }
+    function f11 { npm.has_dev_dependency "jest" && echo "-D jest-environment-jsdom"; }
+    function f12 { npm.has_dev_dependency "eslint" && echo "-D $(printf "eslint-plugin-%s " "$ESLINT_PLUGINS")"; }
 
     function callback {
         local args; args="$("$1")"

e06c524c9ea5125281e33cabd726b6fd0a96dfdd
Matt Riley <mattrileyaus@gmail.com>
2022-09-09 10:07:32 +1000

    Unspecified changes

--- src/bash/modules/node/exec-module.sh
+++ src/bash/modules/node/exec-module.sh
@@ -5,6 +5,7 @@ function node.exec_module {
     node \
         --experimental-specifier-resolution="node" \
         --input-type="module" \
+        --no-warnings \
         - < "$TASK_LIBRARY_ROOT/src/node/bin/$1" "${@:2}"
 
 }

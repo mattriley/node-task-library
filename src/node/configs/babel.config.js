4cf26513b0c946751aa1d3e93dc17a48fc1fe1cd
Matt Riley <m@ttriley.dev>
2022-09-30 22:11:51 +1000

    Unspecified changes

--- src/node/configs/babel.config.js
+++ src/node/configs/babel.config.js
@@ -2,8 +2,8 @@ const process = require('process');
 const omit = (process.env.BABEL_OMIT_PRESETS ?? '').split(process.env.SEP);
 
 const presets = [
-    ['@babel/preset-env', { 'targets': 'defaults' }],
-    ['@babel/preset-react', { 'runtime': 'automatic' }]
+    ['@babel/preset-env', { targets: 'defaults', modules: false }],
+    ['@babel/preset-react', { runtime: 'automatic' }]
 ].filter(([name]) => !omit.includes(name));
 
 module.exports = { presets };

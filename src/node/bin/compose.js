b5976518715ddae712f769a8360f396f79093e71
Matt Riley <m@ttriley.dev>
2022-09-30 22:31:34 +1000

    Unspecified changes

--- src/node/bin/compose.js
+++ src/node/bin/compose.js
@@ -1,7 +1,8 @@
 const process = require('process');
+const path = require('path');
 
 const main = async () => {
-    const imported = await import(process.env.COMPOSE);
+    const imported = await import(path.resolve(process.env.COMPOSE));
     const compose = imported?.default ?? imported;
     const { load, json } = compose();
     if (load) await load();

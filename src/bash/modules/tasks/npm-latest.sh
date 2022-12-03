df0aae5a0c7ec987f4f59b3a8b8a3ebb5ebf364e
Matt Riley <m@ttriley.dev>
2022-12-04 10:43:40 +1100

    Unspecified changes

--- src/bash/modules/tasks/npm-latest.sh
+++ src/bash/modules/tasks/npm-latest.sh
@@ -2,11 +2,19 @@
 
 function tasks.npm_latest {
 
+    local args; args=${1:-"-u"}
+    local package_json_before; package_json_before=$(<"$PACKAGE_JSON")
+
     reporter.newline && \
-    ncu -u && \
-    reporter.newline && \
-    npm.install && 
-    npm.install "-D task-library" && \
+    ncu "$args" && \
     reporter.newline
+
+    local package_json_after; package_json_after=$(<"$PACKAGE_JSON")
+    [ "$package_json_after" = "$package_json_before" ] && return
+
+    npm.install && \
+    reporter.newline
+
+    # npm.install "-D task-library" && \
 
 }

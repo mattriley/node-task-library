6233216b0780dd1a6240d7b68425c67127474eb4
Matt Riley <m@ttriley.dev>
2023-02-24 14:23:02 +1100

    Unspecified changes

--- src/bash/modules/tasks/ls.sh
+++ src/bash/modules/tasks/ls.sh
@@ -3,6 +3,15 @@
 function tasks.ls {
 
     local list; list=$(compgen -A function | grep "tasks.")
-    echo "${list//tasks./}"
+
+    for val in $list; do
+
+        IFS='.' read -ra arr <<< "$val"
+        local task_name; task_name="${arr[1]}"
+        local infer_func; infer_func="tasks.$task_name.infer"
+        local inferred; util.is_function "$infer_func" && inferred="$($infer_func)"
+        echo "$task_name - $inferred"
+
+    done
 
 }

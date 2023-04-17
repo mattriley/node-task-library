aae8fc275740c7483ae1528f7d15e088d6e44ecc
Matt Riley <m@ttriley.dev>
2023-04-17 17:09:24 +1000

    Unspecified changes

--- src/bash/modules/infer/lint.sh
+++ src/bash/modules/infer/lint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# shellcheck disable=SC2317
 
 function infer.lint {
 
     function f1 { npm.has_dev_dependency "eslint" && echo "eslint"; }
+    function f2 { which -s shellcheck && echo "shellcheck"; }
     lib.infer_tasks
 
 }

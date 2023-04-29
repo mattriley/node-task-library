180904de0e68b0863883466c86015df13cbfc229
Matt Riley <m@ttriley.dev>
2023-04-30 09:32:28 +1000

    Unspecified changes

--- src/bash/modules/tasks/metrics-summary.sh
+++ src/bash/modules/tasks/metrics-summary.sh
@@ -3,7 +3,7 @@
 function tasks.metrics_summary {
 
     mkdir -p "$METRICS" && \
-    ( [ -f "$COVERAGE_JSON_SUMMARY" ] && cp "$COVERAGE_JSON_SUMMARY" "$METRICS_COV" ) && \
+    ( [ -f "$COVERAGE_JSON_SUMMARY" ] && cp "$COVERAGE_JSON_SUMMARY" "$METRICS_COV"; return 0 ) && \
     node.exec_script "metrics-summary.js" > "$METRICS_SUMMARY"
 
 }

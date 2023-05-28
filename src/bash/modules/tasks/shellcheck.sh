b241c38de172a24598fb8b93a97c6a11c240c653
Matt Riley <m@ttriley.dev>
2023-05-28 11:57:49 +1000

    Unspecified changes

--- src/bash/modules/tasks/shellcheck.sh
+++ src/bash/modules/tasks/shellcheck.sh
@@ -5,7 +5,8 @@ function tasks.shellcheck {
     # shellcheck disable=SC2153
     local paths; paths=$(lib.filter_dir_exists "$SHELLCHECK_PATHS")
 
-    # shellcheck disable=SC2038,2068,2086
-    find $paths -name "*.sh" | xargs shellcheck $@
+    # shellcheck disable=SC2317
+    function shellcheck_callback { find "$1" -type f -name "*.sh" -exec shellcheck "{}" \; ; }
+    list.each "$paths" shellcheck_callback
 
 }

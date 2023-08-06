a68e00d29cf475ff6364e43ca17aa1e402ab4552
Matt Riley <m@ttriley.dev>
2023-08-07 07:07:05 +1000

    Unspecified changes

--- src/bash/modules/infer/dist.sh
+++ src/bash/modules/infer/dist.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# shellcheck disable=2317
 
 function infer.dist {
 
-    function f1 { node.is_module_installed "parcel" && echo "parcel-dist"; }
-    lib.infer_tasks
+    function infer_dist_1 { node.is_module_installed "parcel" && echo "parcel-dist"; }
+    lib.infer_tasks infer_dist_
 
 }

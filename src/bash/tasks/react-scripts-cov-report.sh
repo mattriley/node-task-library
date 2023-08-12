e2ae395fbe39ea4162f1d3fdd7a18785048f239b
Matt Riley <m@ttriley.dev>
2023-08-12 11:10:52 +1000

    Unspecified changes

--- src/bash/tasks/react-scripts-cov-report.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function tasks.react_scripts_cov_report {
-
-    # echo "$(node $TASK_LIBRARY_ROOT/src/node/bin/configure-jest-coverage-reporters.js)" > ./package.json
-
-    task_runner.run_task "react-scripts-test" --coverage "$@"
-
-}
--- src/bash/tasks/react-scripts-test-watch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-function tasks.react_scripts_test_watch {
-
-    react-scripts test
-
-}

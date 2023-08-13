65b61a144bb27b83cc0807aec981fe5b5b67cf8c
Matt Riley <m@ttriley.dev>
2023-08-13 13:35:29 +1000

    Unspecified changes

--- src/bash/modules/infer/config.sh
+++ src/bash/modules/infer/config.sh
@@ -5,7 +5,7 @@ function infer.config {
 
     function infer_config_1 { bool.true "$GIT_DETECTED" && echo "gitignore-gen"; }
     function infer_config_2 { bool.true "$PACKAGE_JSON_DETECTED" && echo "package-gen"; }
-    function infer_config_3 { bool.true "$REACT_DETECTED" && echo "babel-config-gen"; }
+    function infer_config_3 { bool.true "$REACT_DETECTED" && bool.false "$PARCEL_DETECTED" && echo "babel-config-gen"; }
     function infer_config_4 { bool.true "$JEST_DETECTED" && echo "jest-config-gen"; }
     function infer_config_5 { bool.true "$ESLINT_DETECTED" && echo "eslint-config-gen"; }
     task_runner.infer_tasks infer_config_

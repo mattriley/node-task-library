9a947b27a9974b383d179ce589d60991a813d965
Matt Riley <m@ttriley.dev>
2023-08-15 19:06:25 +1000

    Unspecified changes

--- src/bash/modules/aws/ssm.sh
+++ src/bash/modules/aws/ssm.sh
@@ -2,8 +2,6 @@
 
 function aws.ssm {
 
-    # aws ssm get-parameter --region "$AWS_REGION" --name "$1" --with-decryption | tools.jq -r '.Parameter.Value'
-
     aws ssm get-parameter --region "$AWS_REGION" --name "$1" --with-decryption | json.get 'Parameter.Value'
 
 }
--- src/bash/modules/tools/jq.sh
+++ src/bash/modules/tools/jq.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# NOTE: Currently not in use. Try to avoid the extra dependency. Use `json.get` instead.
+
 function tools.jq {
 
     which -s "jq" || tools.brew install "jq"

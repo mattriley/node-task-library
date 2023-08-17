cf3e0da46028f04687d76bae3e28f51997f54f6a
Matt Riley <m@ttriley.dev>
2023-08-18 08:47:37 +1000

    Unspecified changes

--- src/bash/modules/node/exec-infer.sh
+++ src/bash/modules/node/exec-infer.sh
@@ -4,14 +4,14 @@
 function node.exec_infer {
 
     if node.module_installed "@node-loader/babel"; then
-        export NODE_OPTIONS="--experimental-loader @node-loader/babel --no-warnings"
+        export NODE_OPTIONS="$NODE_OPTIONS --experimental-loader @node-loader/babel --no-warnings"
         node.exec_script "$@"
-        return
+        return $?
     fi
 
     if node.module_installed "@babel/node"; then
         node.exec_babel "$@"
-        return
+        return $?
     fi
 
     node.exec_script "$@"
--- src/bash/tasks/module-testrunner-test.sh
+++ src/bash/tasks/module-testrunner-test.sh
@@ -3,6 +3,6 @@
 function tasks.module_testrunner_test {
 
     # shellcheck disable=2046
-    NODE_OPTIONS="$NODE_OPTIONS $*" mtr $(tests.list_tests)
+    NODE_OPTIONS="$NODE_OPTIONS $*" node.exec_infer "./$NODE_MODULES/module-testrunner/bin/test" $(tests.list_tests)
 
 }

20f75f30060c07ec4b9142b05686e9305c96a65f
Matt Riley <m@ttriley.dev>
2023-08-18 21:13:15 +1000

    Unspecified changes

--- src/bash/tasks/module-testrunner-test.sh
+++ src/bash/tasks/module-testrunner-test.sh
@@ -2,7 +2,9 @@
 
 function tasks.module_testrunner_test {
 
-    # shellcheck disable=2046
-    NODE_OPTIONS="$NODE_OPTIONS $*" node.exec_infer "./$NODE_MODULES/module-testrunner/bin/test" $(tests.list_tests)
+    local node_options; node.module_installed 'global-jsdom' && node_options="-r global-jsdom/register"
+
+    # shellcheck disable=2046,2153
+    NODE_OPTIONS="$NODE_OPTIONS $node_options $*" node.exec_infer "./$NODE_MODULES/module-testrunner/bin/test" $(tests.list_tests)
 
 }

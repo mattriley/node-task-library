c6be74fe85dca1c55286fb12660967287d487eb2
Matt Riley <m@ttriley.dev>
2023-08-20 21:54:50 +1000

    Unspecified changes

--- src/bash/modules/list/find-invoke.sh
+++ src/bash/modules/list/find-invoke.sh
@@ -9,25 +9,9 @@ function list.find_invoke {
     for fun in "${arr[@]}"; do
         local val; val=$(fp.invoke "$fun")
         local res; res=$(fp.invoke "$callback" "$val")
-        [ -n "$found" ] && found="$res"
+        [ -n "$res" ] && [ -z "$found" ] && found="$res"
     done
 
     echo "$found"
 
 }
-
-
-# #!/bin/bash
-
-# function list.find_invoke {
-
-#     read -r -a arr <<< "$1"
-#     local callback="$2"
-
-#     for fun in "${arr[@]}"; do
-#         local val; val=$(fp.invoke "$fun")
-#         local res; res=$(fp.invoke "$callback" "$val")
-#         [ "$res" ] && echo "$res" && break;
-#     done
-
-# }
